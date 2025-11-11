e79c5ec265e51481c79035616ef9de5ab6a3602c
Atalay52 <148344381+Atalay52@users.noreply.github.com>
2025-11-11 12:37:31 +0300

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Eğer basePath kullanıyorsan burayı doğru ayarla; yoksa kaldır
+  // basePath: '/some-basepath',
+  // trailingSlash: false,
   reactStrictMode: true,
 };
 
